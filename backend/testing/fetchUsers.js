6a5b87193f82471687dc0b320638d7b28951df5c
Ahmad <ahmadshaukat328@gmail.com>
2025-03-15 10:59:41 -0400

    testing

--- backend/testing/fetchUsers.js
+++ backend/testing/fetchUsers.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import User from "../models/User.js";
 import connectDB from "../config/db.js";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 const getAllUsers = async () => {
   try {
