ad9af6cf7eb5764c0b5ce58ee12f661301027f44
Ahmad <ahmadshaukat328@gmail.com>
2025-03-22 18:56:41 -0400

    anime card backend

--- frontend/src/Anime.jsx
+++ frontend/src/Anime.jsx
@@ -3,14 +3,13 @@ import AnimeCard from "./Animecard";
 
 const Anime = () => {
     const [anime, setAnime] = useState(null);
-    let n = "onepiece";
 
     const getAnime = async () => {
         try {
             const response = await fetch("http://localhost:5000/api/auth/findanime", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ name: n }),
+                body: JSON.stringify({ animeid: 1 }),
             });
 
             const data = await response.json();
