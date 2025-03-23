f2ae2f43f48dc433b5b65131e736137a25a92f8a
Ahmad <ahmadshaukat328@gmail.com>
2025-03-23 11:32:05 -0400

    nothing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Signup from './Signup';
 import Login from './Login';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Anime from './Anime';
+import Header from './Header';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path='/header' element={<Header/>}/>
         <Route path="/x" element={<Anime />} />
       </Routes>
     </BrowserRouter>
