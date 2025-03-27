67688ccbe4c70bfdaf74d6424ac5ac147e4872e7
Ahmad <ahmadshaukat328@gmail.com>
2025-03-27 13:45:07 -0400

    db improvements

--- backend/models/animeSchema.js
+++ backend/models/animeSchema.js
@@ -1,18 +1,20 @@
 import mongoose from "mongoose";
+
 const animeSchema = new mongoose.Schema({
-    animeid: { type: Number, required: true, unique: true },
-    animename: { type: String, required : true, unique: true },
-    genre: { type: String, required: true },
-    rating: { type: Number, required: true },
-    episodes: { type: Number, required: true },
-    popularity: { type: Number, required: true },
-    image: { type: String, required: true },
-    description: { type: String},
-    trailer: { type: String, required: true },
-    status: { type: String, required: true },
-    season: { type: String, required: true },
-    source: { type: String, required: true },
-    character: { type: [String], required: true },  
+  animeid: { type: Number, required: true, unique: true },
+  anime_name: { type: String, required: true, unique: true },
+  genre: { type: String, required: true },
+  rating: { type: Number, required: true },
+  episodes: { type: Number, required: true },
+  popularity: { type: Number, required: true },
+  image: { type: String, required: true },
+  description: { type: String },
+  trailer: { type: String, required: true },
+  status: { type: String, required: true },
+  season: { type: String, required: true },
+  source: { type: String, required: true },
+  characters: { type: [String], required: true },
 }, { timestamps: true });
+
 const Anime = mongoose.model("Anime", animeSchema);
 export default Anime;
