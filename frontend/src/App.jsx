fbc8af435dee0dc962ffbe04caa0962684270e24
Muteeb <l227898@lhr.nu.edu.pk>
2025-04-18 20:24:21 +0500

    NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import Signup from './Signup';
-import Login from './Login';
+import Signup from './SignUpPage/Signup';
+import Login from './LoginPage/Login';
+import Navbar from './NavBar/Navbar';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Anime from './Anime';
-import Header from './Header';
-
 function App() {
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path='/header' element={<Header/>}/>
+
         <Route path="/x" element={<Anime />} />
       </Routes>
     </BrowserRouter>
+  
   );
 }
 
---
+++ frontend/src/LoginPage/Login.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Login.css";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const response = await fetch("http://localhost:5000/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || "Login failed");
+      }
+
+      alert("Login successful");
+      navigate("/Login"); 
+    } catch (error) {
+      console.error("Login Error:", error.message);
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Sign In</h2>
+        <form onSubmit={handleLogin}>
+          <div className="input-group">
+            <input
+              type="text"
+              id="username"
+              placeholder="Email or Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-group">
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" disabled={loading} id = "login-btn">
+            {loading ? "Signing In..." : "Sign In"}
+          </button>
+          <div className="form-footer">
+            <p>New to AniSync? <Link to={"/signup"}>Sign up now</Link></p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
