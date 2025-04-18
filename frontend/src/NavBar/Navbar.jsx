7be0d7480730674bcc29973fec742115556b346e
Muteeb <l227898@lhr.nu.edu.pk>
2025-04-18 20:57:04 +0500

    navbar

--- frontend/src/NavBar/Navbar.jsx
+++ frontend/src/NavBar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import './Navbar.css'; // Make sure to import the CSS
+import './Navbar.css'; 
 
 const Navbar = () => {
   return (
