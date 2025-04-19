69c760c0975c904e94440743d36598943c1bfd38
Ahmad <ahmadshaukat328@gmail.com>
2025-04-19 11:30:12 -0400

    s3

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
+import Store from "./Redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  
+    <Provider store={Store}>
     <App />
+    </Provider>
 
 );
