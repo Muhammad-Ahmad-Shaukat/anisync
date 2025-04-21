7f51acee3c81aec418582b14f7940b4807f59027
Muteeb <l227898@lhr.nu.edu.pk>
2025-04-21 14:11:21 +0500

    list

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import Signup from './SignUpPage/Signup';
-import Login from './LoginPage/Login';
-import Navbar from './NavBar/Navbar';
+import Signup from './Pages/SignUpPage/Signup';
+import Login from './Pages/LoginPage/Login';
+import Navbar from './components/NavBar/Navbar';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
-import Slider from './Homepage/Slider'; 
-import Card from './Homepage/AnimeCard'; 
 
+import AnimeList from './components/AnimeList/AnimeList';
 
 function App() {
   return (
-    <>
-     <BrowserRouter>
-      <Navbar/>
-      <Card/>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+    <AnimeList/>
+    // <>
+    //  <BrowserRouter>
+    //   <Navbar/>
+    //   <Card/>
+    //   <Routes>
+    //     <Route path="/signup" element={<Signup />} />
+    //     <Route path="/login" element={<Login />} />
       
-      </Routes>
-    </BrowserRouter>
-    </>
+    //   </Routes>
+    // </BrowserRouter>
+    // </>
   );
 }
 
--- frontend/src/Pages/LoginPage/Login.jsx
+++ frontend/src/Pages/LoginPage/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { loginSuccess } from "../Redux/authslice"; 
+import { loginSuccess } from "../../components/Redux/authslice"
 import "./Login.css";
 
 const Login = () => {
