b75e335b3d9bc02edf03342469a9250fc671f723
Ahmad <ahmadshaukat328@gmail.com>
2025-04-28 07:28:30 -0400

    Removes commented-out fetch logic for top anime
    Cleans up the controller by removing unused code that fetches top anime data. This helps improve code readability and maintainability.
    
    No functional changes were made to the current implementation.

--- backend/Controllers/topAnimeController.js
+++ backend/Controllers/topAnimeController.js
@@ -24,3 +24,36 @@ export const getTopAnime = async (req, res) => {
     res.status(500).json({ error: "Failed to fetch top anime" });
   }
 };
+
+
+// import fetch from "node-fetch";
+
+// export const getTopAnime = async (req, res) => {
+//   const { limit, action } =req.query;
+//   if (!limit || !action) {
+//     return res.status(400).json({ message: "Limit and action is required." });
+//   }
+//   try{
+//     const response = await fetch(`https://api.jikan.moe/v4/top/anime?limit=${limit}&action=${action}`);
+//     const data = await response.json();
+
+//     const animeList = data.data.map((anime) => ({
+//       id: anime.mal_id,
+//       title: {
+//         english: anime.title_english,
+//         romaji: anime.title,
+//       },
+//       description: anime.synopsis,
+//       bannerImage:   anime.trailer?.images?.maximum_image_url || anime.images?.jpg?.large_image_url ,
+//       coverImage: {
+//         large: anime.images?.jpg?.large_image_url,
+//       },
+//     }));
+
+//     res.json(animeList);
+
+//   }catch (error) {
+//     console.error("Failed to fetch top anime:", error);
+//     res.status(500).json({ error: "Failed to fetch top anime" });
+//   }
+// }
