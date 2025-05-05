423704c785016c97c42278096d7d0e7ad3f9a6b8
Ahmad <ahmadshaukat328@gmail.com>
2025-05-05 04:48:35 -0400

    Updates 404 page content
    Improves user experience by adding relevant text on the 404 page

--- frontend/src/Pages/404Page/404Page.jsx
+++ frontend/src/Pages/404Page/404Page.jsx
@@ -7,7 +7,7 @@ const NotFoundPage = () => {
             <img src="" alt="" />
             <h3>Awww... Don't Cry</h3>
             <h4>Its Just a 404 Error</h4>
-            <h5></h5>
+            <h5>What You are Looking</h5>
         </div>
         </>
     )
