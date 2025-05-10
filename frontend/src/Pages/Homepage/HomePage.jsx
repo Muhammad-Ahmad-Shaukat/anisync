1f37dc2eb7de8f715b18da094a33ab650edad9a3
ZainMalik0405 <zainmalikk0907@gmail.com>
2025-05-11 01:05:27 +0500

    Admin Edit Page UI

--- frontend/src/Pages/Homepage/HomePage.jsx
+++ frontend/src/Pages/Homepage/HomePage.jsx
@@ -2,6 +2,7 @@ import AnimeList from "../../components/AnimeList/AnimeList";
 import AnimeSlider from "../../components/AnimeSlider/AnimeSlider";
 import AdvancedSearch from "../../components/AdvanceSearch/AdvancedSearch";
 import AdminAnimeList from "../AdminPage/AdminAnimeList";
+import AnimeEditPage from "../AdminPage/AnimeEditPage";
 
 
 function HomePage(){
@@ -11,11 +12,11 @@ function HomePage(){
     return<>
     
  
-        <AnimeList/>
+        {/* <AnimeList/>
         <AnimeSlider type="Trending" limit={11} />
         <AnimeSlider type="top" limit={11} />
-        <AnimeSlider type="airing" limit={11} />
-    {/* <AdminAnimeList/> */}
+        <AnimeSlider type="airing" limit={11} /> */}
+        <AnimeEditPage/>
     </>
 }
 
