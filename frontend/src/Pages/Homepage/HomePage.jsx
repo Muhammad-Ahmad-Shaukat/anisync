5a7bad0c751d12467b02b7eb28cf3522bd46bf3a
Ahmad <ahmadshaukat328@gmail.com>
2025-05-10 16:27:25 -0400

    d

--- frontend/src/Pages/Homepage/HomePage.jsx
+++ frontend/src/Pages/Homepage/HomePage.jsx
@@ -17,6 +17,7 @@ function HomePage(){
         <AnimeSlider type="top" limit={11} />
         <AnimeSlider type="airing" limit={11} />
         {/* <AnimeEditPage/> */}
+        <AdminAnimeList/>
     </>
 }
 
